75118bd835639c978dc8563b96bb663416e65989
Brandi Anderson <branderso87@gmail.com>
2017-07-06 22:09:08 -0500

    add new novels js created

--- routes/addnovels.js
+++ routes/addnovels.js
@@ -3,9 +3,29 @@ const router = express.Router()
 const Novel = require('../models/novels')
 
 router.get('/novels/new', function (req, res) {
-  res.render ('addnovels')
+  res.render('novels/new')
 })
 
+router.post('/novels', function (req, res) {
+  const novel = new Novel()
+  novel.name = req.body.title
+  novel.author = req.body.author
+  novel.pub = req.body.pub
+  novel.imgurl = req.body.imagelink
+  novel.buyurl = req.body.buylink
+  novel.characters.mainFemale = req.body.female_main
+  novel.characters.supportFemale = req.body.female_support
+  novel.characters.male = req.body.male
 
+  novel.save()
+  .then(function (novel) {
+    res.json(novel)
+  })
+}).catch(function (validationError) {
+    res.render('novels/new', {
+      novel: novel,
+      valdiationError: validationError
+    })
+})
 
 module.exports = router
